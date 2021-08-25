44e3e2af9667fd8871fb3bf6dbd8a25e5acf4ffd
James Bernardi <jbernardi@forumone.com>
2021-08-25 15:07:49 -0400

    typo

--- smallstep/files/okta-sync.sh
+++ smallstep/files/okta-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-TOKEN=$(aws --region us-east-2 ssm get-parameter --name "/forumone/okta/read_only_okta_token" --with-decryption | jq -r .Parameter.Value')
+TOKEN=$(aws --region us-east-2 ssm get-parameter --name "/forumone/okta/read_only_okta_token" --with-decryption | jq -r '.Parameter.Value')
 
 ID=$(curl -s -X GET \
 -H "Accept: application/json" \
